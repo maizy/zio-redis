5ab5a6e85303248438266cb122f3e72178e332e8
Nikita Kovalev <nikita@maizy.dev>
2024-08-18 13:07:03 +0200

    [wip] example 2

--- modules/example2/src/main/scala/zio/redis/example2/Main.scala
+++ modules/example2/src/main/scala/zio/redis/example2/Main.scala
@@ -5,26 +5,40 @@ import zio.redis._
 
 object Main extends ZIOAppDefault {
 
-  private val redisRoutine: ZIO[Redis, RedisError, Unit] = for {
-    redis    <- ZIO.service[Redis]
-    key       = "test"
+  private def redisRoutine(id: String): ZIO[Redis, RedisError, String] = (for {
+    redis    <- ZIO.service[Redis] <* ZIO.log(s"[$id] service is ready")
+    sleep    <- Random.nextIntBetween(1, 10).map(_.seconds)
+    _        <- ZIO.log(s"[$id] sleep for $sleep") *> Clock.sleep(sleep)
+    key       = s"test:$id"
     thisRun  <- Clock.currentDateTime
     before   <- redis.get(key).returning[Long]
                   .catchAllDefect {
-                    e => ZIO.log(s"fallback for $key because of defect $e").as(Option.empty[Long])
+                    e => ZIO.log(s"[$id] fallback for $key because of defect $e").as(Option.empty[Long])
                   }
                   .catchAll {
-                    e => ZIO.log(s"fallback for $key because of error $e").as(Option.empty[Long])
+                    e => ZIO.log(s"[$id] fallback for $key because of error $e").as(Option.empty[Long])
                   }
-    _        <- Console.printLine(s"Value of $key before: $before").orDie
-    _        <- redis.set(key, thisRun.toInstant.toEpochMilli)
+    _        <- ZIO.log(s"[$id] Value of $key before: $before")
+    _        <- redis.set(key, thisRun.toInstant.toEpochMilli, expireTime = Some(5.seconds))
     after    <- redis.get(key).returning[Long]
-    _        <- Console.printLine(s"Value of $key after: $after").orDie
-  } yield ()
+    _        <- ZIO.log(s"[$id] Value of $key after: $after")
+  } yield id).onInterrupt(onInterruptRedis(id))
 
+  private def onInterruptRedis(id: String) = (fibers: Set[FiberId])
+    => ZIO.log(s"interrupt $id: fibers=[${fibers.map(_.threadName).mkString(";")}]")
+
+
+  private val scatterGather: ZIO[Redis, RedisError, String] =
+    redisRoutine("a") race redisRoutine("b") race redisRoutine("c")
 
   def run: ZIO[ZIOAppArgs with Scope, Any, Any] =
-    redisRoutine.provide(
+    (for {
+      _       <- ZIO.log("start")
+      win1   <- scatterGather
+      win2   <- scatterGather
+      win3   <- scatterGather
+      _ <- ZIO.log(s"res: $win1 $win2 $win3")
+    } yield ()).provide(
       Redis.singleNode.tapError {
         e => ZIO.log(s"redis connection error: $e")
       },
